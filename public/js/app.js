d9d43392db4928754e8a72852fe59db4cd208d07
Dhiren <singh.dhirendra07@gmail.com>
2020-06-13 23:14:09 +0530

    new changes to push in heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,20 +3,24 @@ console.log('Client side javascript file is loaded!')
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
 
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
     fetch('/weather?location=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                messageOne.textContent = data.error
             } else {
-                console.log(data.address)
-                console.log(data.forecast)
-                console.log(data.longitude)
-                console.log(data.lattitude)
+                messageOne.textContent = data.address
+                messageTwo.textContent = data.forecast
             }
         })
     })
